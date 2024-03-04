2793695734cfba98fa3bfd5f559a784a5d5be1c9
Moradens <denis.moravec@email.cz>
2024-03-04 12:29:35 +0100

    .

--- Fraction.java
+++ Fraction.java
@@ -1,10 +1,5 @@
-public final class Fraction {
-        public static void main(String[] args) {
-            Fraction a = Fraction.of(8, 7);
-            Fraction quarter = Fraction.of(1, 4);
+public final class Fraction implements Comparable<Fraction> {
 
-            System.out.println(a.multiply(quarter).toReal());
-        }
     private final int numerator;
     private final int denominator;
 
@@ -56,10 +51,55 @@ public final class Fraction {
         return this.numerator + "/" + this.denominator;
     }
 
+    @Override
+    public int compareTo(Fraction other) {
+        return Integer.compare(this.numerator * other.denominator, this.denominator * other.numerator);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || !(other instanceof Fraction)) {
+            return false;
+        }
+        Fraction that = (Fraction) other;
+        return this.numerator == that.numerator && this.denominator == that.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.numerator * 31) + this.denominator;
+    }
+
     private static int gcd(int a, int b) {
         if (b == 0) {
             return a;
         }
         return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Fraction fraction1 = Fraction.of(1, 2);
+        Fraction fraction2 = Fraction.of(3, 4);
+
+        Fraction sum = fraction1.add(fraction2);
+        System.out.println("Sum: " + sum);
+
+        Fraction difference = fraction1.subtract(fraction2);
+        System.out.println("Difference: " + difference);
+
+        Fraction product = fraction1.multiply(fraction2);
+        System.out.println("Product: " + product);
+
+        Fraction quotient = fraction1.divide(fraction2);
+        System.out.println("Quotient: " + quotient);
+
+        Fraction reciprocal = fraction1.reciprocal();
+        System.out.println("Reciprocal of fraction1: " + reciprocal);
+
+        double realValue = fraction1.toReal();
+        System.out.println("Real value of fraction1: " + realValue);
     }
 }
